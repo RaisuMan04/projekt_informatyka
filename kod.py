6dca48991272432e2dbf73c7181e8399ce869ba6
Adam <orzeszek1015@gmail.com>
2024-05-07 10:18:13 +0200

    w sumie to nic nowego, poprawienie tego co już było

--- kod.py
+++ kod.py
@@ -4,9 +4,8 @@ from math import *
 
 o = object()
 
-class Transformacje:
-    
-    def __init__(self, model: str = "wgs84"):
+class Transformacje:  
+    def __init__(self, model):
         """
         """
         if model == "wgs84":
@@ -17,33 +16,52 @@ class Transformacje:
             self.b = 6356752.31414036
         elif model == "krasowski":
             self.a = 6378245.0
-            self.b = 6378102.119674574
+            self.b = 6356863.019
         else:
             raise NotImplementedError(f"{model} model not implemented")
         self.flat = (self.a - self.b) / self.a
         self.ecc = sqrt(2 * self.flat - self.flat ** 2) 
         self.ecc2 = (2 * self.flat - self.flat ** 2) 
         
-    def xyz2plh(self, X, Y, Z, output = "dec_degree"):
+    # jednostki
+    jednostka = sys.argv[2]
+    if jednostka == "bez":
+        pass
+    else:
+        output = jednostka
+        
+    #z radianów na stopnie
+    def rad2dms(x):
+        sig = ' '
+        if x<0:
+            sig = '-'
+            x = abs(x)
+        x = x * 180 / pi
+        d = int(x)
+        m = int(60 * (x - d))
+        s = (x - d - m/60) * 3600
+        return(f'{sig}{d:3d}{chr(176)}{abs(m):2d}\'{abs(s):7.5f}\"')
+        
+    def xyz2plh(self, X, Y, Z, output = jednostka):
         """
         """
         r = sqrt(X**2 + Y**2)           
-        lat_prev = atan(Z / (r * (1 - self.ecc2)))    
-        lat = 0
-        while abs(lat_prev - lat) > 0.000001/206265:    
-            lat_prev = lat
-            N = self.a / sqrt(1 - self.ecc2 * sin(lat_prev)**2)
-            h = r / cos(lat_prev) - N
-            lat = atan((Z/r) * (((1 - self.ecc2 * N/(N + h))**(-1))))
-        lon = atan(Y/X)
-        N = self.a / sqrt(1 - self.ecc2 * (sin(lat))**2);
-        h = r / cos(lat) - N       
+        p_0 = atan(Z / (r * (1 - self.ecc2)))    
+        p = 0
+        while abs(p_0 - p) > 0.000001/206265:    
+            p_0 = p
+            N = self.a / sqrt(1 - self.ecc2 * sin(p_0)**2)
+            h = r / cos(p_0) - N
+            p = atan((Z/r) * (((1 - self.ecc2 * N/(N + h))**(-1))))
+        l = atan(Y/X)
+        N = self.a / sqrt(1 - self.ecc2 * (sin(p))**2);
+        h = r / cos(p) - N       
         if output == "dec_degree":
-            return degrees(lat), degrees(lon), h 
+            return degrees(p), degrees(l), h 
         elif output == "dms":
-            lat = rad2dms(degrees(lat))
-            lon = rad2dms(degrees(lon))
-            return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
+            p = rad2dms(degrees(p))
+            l = rad2dms(degrees(l))
+            return p, l, h
         else:
             raise NotImplementedError(f"{output} - output format not defined")
             
@@ -58,9 +76,12 @@ class Transformacje:
     
 if __name__ == "__main__":
     # utworzenie obiektu
-    geo = Transformacje(model = "wgs84")
+    geo = Transformacje(model = sys.argv[3])
     # dane XYZ geocentryczne
-    with open('wsp_inp.txt', 'r') as t:
+    plik = sys.argv[4]
+    method = sys.argv[1]
+    
+    with open(plik, 'r') as t:
         text = t.readlines()
         dane = text[4:]
         wsp = []
@@ -77,11 +98,14 @@ if __name__ == "__main__":
             phi, lam, h = geo.xyz2plh(X[i], Y[i], Z[i])
             wyniki.append([phi, lam, h])
         wyniki = np.array(wyniki)
-            
-    with open('raport_xyz2plh.txt', 'w') as p:
-        p.write('      phi      |       lam      |  h [m]     \n')
-        for phi, lam, h in wyniki:
-            p.write(f'{phi:.12f}  {lam:.12f}  {h:.3f} \n')
+        
+    # gdy plik ze wsp zawiera wsp. XYZ
+    if method == "xyz2plh":
+        with open('raport_xyz2plh.txt', 'w') as p:
+            p.write('      phi      |       lam      |  h [m]     \n')
+            for phi, lam, h in wyniki:
+                p.write(f'{phi:.12f}  {lam:.12f}  {h:.3f} \n')
+    # gdy plik ze wsp zawiera wsp. PLH
             
     with open('raport_xyz2plh.txt', 'r') as l:
         text = l.readlines()
@@ -100,8 +124,10 @@ if __name__ == "__main__":
             X1, Y1, Z1 = geo.plh2xyz(Phi[i], Lam[i], H[i])
             wyniki2.append([X1, Y1, Z1])
         wyniki2 = np.array(wyniki2)
-        
+            
     with open('raport_plh2xyz.txt', 'w') as p:
         p.write('    X [m]   |    Y [m]   |    Z [m]    \n')
         for X, Y, Z in wyniki2:
             p.write(f'{X:.3f}, {Y:.3f}, {Z:.3f} \n')
+      
+print("Program został wykonany poprawnie :)")
